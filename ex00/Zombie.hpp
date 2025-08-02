69a6f63039ea07aa104ba4e9cac7493f730050bc
daniel <daniel-congo2@hotmail.com>
2025-08-02 04:25:15 +0200

    création des fichiers

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 22:49:04 by daniel149af       #+#    #+#             */
+/*   Updated: 2025/08/01 22:58:50 by daniel149af      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <stdlib.h>
+# include <string.h>
+# include <iostream> // std::string
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		Zombie();
+};
+
+#endif
