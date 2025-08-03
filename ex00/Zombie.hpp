16bc1d2f4b3e16d04fdfe05d2a55f1fe1f7fe58f
daniel <daniel-congo2@hotmail.com>
2025-08-04 01:19:02 +0200

    ex02 en cours

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,14 +6,12 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 22:49:04 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/08/03 23:21:41 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/08/04 00:20:34 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
-# include <stdlib.h>
-# include <string.h>
 # include <iostream> // std::string
 
 class Zombie
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,33 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/01 22:49:04 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/08/04 00:21:06 by daniel149af      ###   ########.fr       */
+/*   Created: 2025/08/04 00:19:33 by daniel149af       #+#    #+#             */
+/*   Updated: 2025/08/04 00:20:09 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-# define ZOMBIE_HPP
-# include <iostream> // std::string
+#include <iostream>
 
-//Zombie Class
-class Zombie
-{
-	private:
-		std::string name;
-	public:
-		Zombie();
-		Zombie(std::string name);
-		~Zombie();
-		
-		void	setName(std::string name);	
-		void	announce(void);
-};
-
-Zombie*	zombieHorde(int N, std::string name);
-
-#endif
