a7e2f01b6f369cbd5e06c18b1dc0715ec489f7ea
daniel <daniel-congo2@hotmail.com>
2025-08-17 23:30:35 +0200

    recherche arg dans line

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,12 +6,12 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 15:14:25 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/08/17 15:54:20 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/08/17 23:27:33 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string.h>
+#include <cstring>
 #include <fstream> //open,
 
 int	main(int argc, char **argv)
@@ -26,5 +26,16 @@ int	main(int argc, char **argv)
 		std::perror(argv[1]);
 		return (1);
 	}
+	else
+	{
+		std::string line;
+		
+		while (std::getline(file, line))
+		{
+			std::strstr(line.c_str(), argv[2]);
+			std::cout << line << std::endl;
+		}
+	}
+	
 	return (0);
 }
