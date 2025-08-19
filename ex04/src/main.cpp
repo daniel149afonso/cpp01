76c2e9081770b4d999f0cf9ceb5556d0a0783d83
daniel <daniel-congo2@hotmail.com>
2025-08-19 16:27:23 +0200

    recherche arg dans line

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,18 +6,27 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 15:14:25 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/08/17 23:52:03 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/08/19 15:40:06 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cstring>
-#include <fstream> //open,
+#include <string>
+#include <cstring> //substr
+#include <fstream> //ifstream, ofstream, open,
+
+// void	replace_file(std::string line, std::ofstream *newFile)
+// {
+	
+// }
 
 int	main(int argc, char **argv)
 {
 	std::ifstream file;
+	std::ofstream fileOut("test.replace");
 	std::string line;
+	std::string word1 = argv[2];
+	std::string word2 = argv[3];
 
 	if (argc != 4)
 		return (1);
@@ -31,10 +40,24 @@ int	main(int argc, char **argv)
 	{
 		while (std::getline(file, line))
 		{
-			if (line.find(argv[2]))
+			int pos = 0;
+			pos = line.find(argv[2], pos);
+			//Parcours toute la ligne pour trouver les occurences
+			while (pos != (int)std::string::npos)
 			{
+				line = line.substr(0, pos) + word2 + line.substr(pos + word1.length());
+				std::cout << "Occurence trouvé: "<< word1 << " à index: "<< pos << std::endl;
+				pos = line.find(word1, pos + word2.length());
 				
-				std::cout << "Occurence trouvé: "<< argv[2] << std::endl;
+			}
+			 std::streampos posCursor = file.tellg(); // position du curseur dans le fichier
+
+			if (posCursor == -1) {
+				// On est à la fin du fichier (pas de \n après cette ligne)
+				fileOut << line;
+			} else {
+				// Il reste encore des données → il y avait un \n
+				fileOut << line << '\n';
 			}
 		}
 	}
