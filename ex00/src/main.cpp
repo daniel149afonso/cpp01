a646c8ce1c863fec59d66aaa915ecbb70a7f4f90
daniel <daniel-congo2@hotmail.com>
2025-08-29 16:42:36 +0200

    projet fini

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,14 +6,12 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 21:42:06 by daniel149af       #+#    #+#             */
-/*   Updated: 2025/08/14 17:44:37 by daniel149af      ###   ########.fr       */
+/*   Updated: 2025/08/29 16:42:11 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Zombie.hpp"
 
-/*Stack obj non-alloué*/
-/*Heap obj alloué dynamiquement*/
 int	main(void)
 {
 	Zombie* zombie;
